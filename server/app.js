755e305a48ea5e92a8ad4d6dbcfc63c4f0948b0a
Tarun Agarwal <its.tarun.2505@gmail.com>
2025-12-12 21:11:15 +0530

    chore(server): Add initial Express project structure

---
+++ server/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+})
